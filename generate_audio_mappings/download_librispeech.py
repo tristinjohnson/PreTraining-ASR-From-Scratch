24c7f6a2feb56dd7222f435f0469ccec5eb1fd2a
tristinjohnson <74263076+tristinjohnson@users.noreply.github.com>
2022-04-06 16:10:37 -0400

    upload script to download LibriSpeech

---
+++ generate_audio_mappings/download_librispeech.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+
+# make a new data directory
+os.makedirs('../../data_test')
+data_dir = '../../data_test/'
+
+# change to data directory
+os.chdir(data_dir)
+
+# get the data split
+parser = argparse.ArgumentParser()
+parser.add_argument('--split', required=True, help='which dataset to download: [dev, train-clean-100, train-clean-360, test]')
+args = parser.parse_args()
+
+# if 'dev' or validation split
+if args.split == 'dev':
+    os.system("sudo wget -c http://www.openslr.org/resources/12/dev-clean.tar.gz")
+    os.system("tar -xvf dev-clean.tar.gz")
+
+# if 'train-clean' 100 hours
+elif args.split == 'train-clean-100':
+    os.system("sudo wget -c http://www.openslr.org/resources/12/train-clean-100.tar.gz")
+    os.system("tar -xvf train-clean-100.tar.gz")
+
+# if 'train-clean' 360 hours
+elif args.split == 'train-clean-360':
+    os.system("sudo wget -c http://www.openslr.org/resources/12/train-clean-360.tar.gz")
+    os.system("tar -xvf train-clean-360.tar.gz")
+
+# if 'test' split
+elif args.split == 'test':
+    os.system("sudo wget -c http://www.openslr.org/resources/12/test-clean.tar.gz")
+    os.system("tar -xvf test-clean.tar.gz")
+
+else:
+    print('Please enter a valid training set')
+
+print('LibriSpeech ' + args.split + ' has successfully been download at: ' + os.getcwd())
