c1d8d0eb3269f8f5df71504636df3099366007f0
tristinjohnson <tejohnson954@gmail.com>
2022-04-27 16:43:00 -0400

    upload script to download RAVDESS dataset and create custom metadata.CSV

--- Code/speech_classification/download_and_map_emotion_data.py
+++ Code/speech_classification/download_and_map_emotion_data.py
@@ -1,18 +1,39 @@
+"""
+Tristin Johnson
+May 2nd, 2022
+
+Script to download the RAVDESS Audio Speech and Songs dataset and to
+generate a .CSV file with metadata about each audio file for custom training methods.
+"""
+# import various python packages
 import os
 from glob import glob
 import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
+data_directory = os.getcwd() + '/classification_data'
+
+# create directory to store songs from RAVDESS
+os.makedirs('classification_data/songs/')
+os.chdir('classification_data/songs/')
+
 # download audio songs link and unzip:
-#os.system("wget -c https://zenodo.org/record/1188976/files/Audio_Song_Actors_01-24.zip")
-#os.system("unzip Audio_Song_Actors_01-24.zip")
-#os.system("rm Audio_Song_Actors_01-24.zip")
+print('Downloading and Decompressing Audio Songs from RAVDESS Emotional Classification')
+os.system("wget -c https://zenodo.org/record/1188976/files/Audio_Song_Actors_01-24.zip")
+os.system("unzip Audio_Song_Actors_01-24.zip")
+os.system("rm Audio_Song_Actors_01-24.zip")
+
+# create directory to store speech from RAVDESS
+os.chdir('../')
+os.makedirs('speech/')
+os.chdir('speech/')
 
 # download audio speech link and unzip:
-#os.system("wget -c wget -c https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip")
-#os.system("unzip Audio_Speech_Actors_01-24.zip")
-#os.system("rm Audio_Speech_Actors_01-24.zip")
+print('Downloading and Decompressing Audio Speech from RAVDESS Emotional Classification')
+os.system("wget -c wget -c https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip")
+os.system("unzip Audio_Speech_Actors_01-24.zip")
+os.system("rm Audio_Speech_Actors_01-24.zip")
 
 
 # function to create df and excel file including audio file name, audio path, and text translation per audio
@@ -39,10 +60,12 @@ def create_csv_audio_mappings(data_directory, data_split_name):
             audio_path = files
             emotion = file[7:8]
 
+            # add metadata attributes to dataframe
             df = df.append({'audio_id': audio_id, 'audio_name': audio_name, 'audio_path': audio_path, 'audio_type': audio_type, 'emotion': emotion}, ignore_index=True)
 
             index += 1
 
+    # drop NaN values and set audio_id as index
     df = df.dropna()
     df['audio_id'] = df['audio_id'].astype(int)
     df.set_index = df['audio_id']
@@ -51,12 +74,15 @@ def create_csv_audio_mappings(data_directory, data_split_name):
     print('Length of Dataframe: ', len(df))
     print('DataFrame columns: ', df.columns)
 
-    df.to_csv('emotional_data_mappings.csv')
+    # export df to .CSV file
+    df.to_csv(data_split_name)
 
     return df
 
 
+# change back to working directory
+os.chdir('../../')
+
 # define data directory and create mappings of emotion dataset
-data_directory = '/home/ubuntu/capstone/classification_data'
 emotion_dataset = create_csv_audio_mappings(data_directory, 'emotional_data_mappings.csv')
 
